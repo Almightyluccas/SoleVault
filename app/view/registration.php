c1adad654012a8070677d5001a27fbb6942b505f
Luccas Amorim <lamorim7657@outlook.com>
2023-06-08 19:30:17 -0400

    Todo List added, fixed dbCreationScript.sql and logoff.php, also added navBar back to registration.php

--- app/view/registration.php
+++ app/view/registration.php
@@ -7,13 +7,12 @@
   <title>SoleVault: Registration</title>
 
 </head>
-<body class="pt-5">
-
+<body class="">
+<?php include 'menu.php'?>
 
 <div class="container-fluid ">
   <div class="row">
     <div class="col-lg-12 text-center" >
-      <h2 class="display-3">Sole Vault: Unleash the Power of Your Soles</h2>
     </div>
   </div>
 </div>
@@ -26,6 +25,7 @@
              class="img-fluid" alt="Phone image">
       </div>
       <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        <h2 class="text-center pb-3">Register Here !</h2>
         <form action='index.php' method='get'>
           <input type='hidden' name='choice' value='register' />
           <div class="form-outline mb-2">
@@ -49,7 +49,7 @@
           <div class="divider  align-items-center my-2">
             <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
           </div>
-          <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="index.php?choice=logon"
+          <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="index.php?choice=login"
              role="button">
             <span>Back to login</span>
           </a>
