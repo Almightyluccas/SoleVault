f6db3a226c0f2dbd2f4146e88db44896cd1dab61
Luccas Amorim <lamorim7657@outlook.com>
2023-06-23 01:11:40 -0400

    Moved Index.php

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,34 +1,33 @@
 <?php
 
-namespace app\core ;
+namespace App\Core;
 
-use app\controller\AboutController;
-use app\controller\AuthenticationController;
-use app\controller\CartController;
-use app\controller\ContactController;
-use app\controller\ErrorController;
-use app\controller\ProductsController;
-use app\controller\RegistrationController;
-use app\core\library\LibraryLG;
-
+use App\Controller\AboutController;
+use App\Controller\AuthenticationController;
+use App\Controller\CartController;
+use App\Controller\ContactController;
+use App\Controller\ErrorController;
+use App\Controller\ProductsController;
+use App\Controller\RegistrationController;
+use App\Core\Library\LibraryLG;
 
 class Router {
   private array $routes = [
     null => [AuthenticationController::class, 'handleLogin'],
-    'login' => [AuthenticationController::class, 'handleLogin'],
-    'logon' => [AuthenticationController::class, 'handleLoginClicked'],
-    'products' => [ProductsController::class, 'handleMainProductPage'],
-    'singleProduct' => [ProductsController::class, 'handleSinglePage'],
-    'cart' => [CartController::class, 'handleCart'],
-    'home' => [ProductsController::class, 'handleHomePage'],
-    'about' => [AboutController::class, 'handleAbout'],
-    'thankyou' => [CartController::class, 'thankYouCheckout'],
-    'contact' => [ContactController::class, 'handleContact', ],
-    'registration' => [RegistrationController::class, 'handleRegister' ],
-    'register' => [RegistrationController::class, 'handleRegister', true],
-    'logoff' => [AuthenticationController::class, 'handleLogOff'],
-    'logoff2' => [AuthenticationController::class, 'handleLogOff', true],
-    'err500' => [ErrorController::class, 'show500Error']
+    '/login' => [AuthenticationController::class, 'handleLogin'],
+    '/logon' => [AuthenticationController::class, 'handleLoginClicked'],
+    '/products' => [ProductsController::class, 'handleMainProductPage'],
+    '/singleProduct' => [ProductsController::class, 'handleSinglePage'],
+    '/cart' => [CartController::class, 'handleCart'],
+    '/home' => [ProductsController::class, 'handleHomePage'],
+    '/about' => [AboutController::class, 'handleAbout'],
+    '/thankyou' => [CartController::class, 'thankYouCheckout'],
+    '/contact' => [ContactController::class, 'handleContact'],
+    '/registration' => [RegistrationController::class, 'handleRegister'],
+    '/register' => [RegistrationController::class, 'handleRegister', true],
+    '/logoff' => [AuthenticationController::class, 'handleLogOff'],
+    '/logoff2' => [AuthenticationController::class, 'handleLogOff', true],
+    '/err500' => [ErrorController::class, 'show500Error']
   ];
 
   public function handleUserRequest(): void {
@@ -50,25 +49,28 @@ class Router {
       $this->handleInvalidRoute();
     }
   }
+
   private function handleInvalidRoute(): void {
     $errorController = new ErrorController();
     $errorController->show404Error();
   }
 
-  public static function redirect( ?array $params = [], ?string $url = 'rocky-thicket-83179-af4910e62a75.herokuapp.com'): void {
+  public static function redirect(?array $params = [], ?string $url = 'https://boiling-spire-92463-d02db77c9c65.herokuapp.com/'): void {
     if (!empty($params)) {
       $queryString = http_build_query($params);
       $url .= '?' . $queryString;
     }
-    header("Location: $url") ;
+    header("Location: $url");
+    exit;
   }
+
   public function addRoute(string $routeName, string $controllerClass, string $method, ?bool $hasParams = false): void {
     $this->routes[$routeName] = [$controllerClass, $method, $hasParams];
   }
+
   public function removeRoute(string $route): void {
     if (isset($this->routes[$route])) {
       unset($this->routes[$route]);
     }
   }
-
 }
